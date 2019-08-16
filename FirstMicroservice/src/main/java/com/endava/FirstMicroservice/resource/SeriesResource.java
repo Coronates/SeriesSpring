a4892a5cb42589f1a43ed5c243988767ebad2cb1
Andres Coronado <acoronado@endava.com>
2019-08-16 16:02:50 -0500

    two queries check

--- FirstMicroservice/src/main/java/com/endava/FirstMicroservice/resource/SeriesResource.java
+++ FirstMicroservice/src/main/java/com/endava/FirstMicroservice/resource/SeriesResource.java
@@ -2,12 +2,12 @@ package com.endava.FirstMicroservice.resource;
 
 import com.endava.FirstMicroservice.model.Serie;
 import com.endava.FirstMicroservice.repository.SeriesRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @RestController
@@ -19,32 +19,50 @@ public class SeriesResource {
         this.serieRepository = serieRepository;
     }
 
+
+    //first method, it returns a list of anime_ids, the method has two optional parameters to filter de anime_ids
+    //by its genre and the number of responses
     @GetMapping("/anime")
-    public List<Integer> getAll(){
-//        List<Serie> series= this.serieRepository.findAll();
-//       List<Integer> idSeries= Stream.of(series).
-//                map(series-> series.contains());
-//        return idSeries;
-
-
-    }
-    @GetMapping("/anime/limit")
-    public List<Serie> getAnimeByLimit(){
+    @ResponseBody
+        public  List<Integer> getAnimeWithLimit(@RequestParam(required = false) Optional<Integer> limit, @RequestParam(required = false) Optional<String> genre) {
         List<Serie> series= this.serieRepository.findAll();
-        return series;
 
+        return series.stream()
+                .filter(x-> x.getGenre().contains(genre.orElseGet(()->"")) )
+                .map(Serie::getAnime_id)
+                .limit(limit.orElseGet(()->series.size()))
+                .collect(Collectors.toList());
+        }
+
+     //this method returns
+    @GetMapping("/anime/{anime_id}")
+      public Serie getSerieById(@PathVariable Integer anime_id){
+          List<Serie> series= this.serieRepository.findAll();
+        return series.stream()
+                .filter(x-> x.getAnime_id().equals(anime_id))
+                .findAny().orElse(null);
 
     }
-    @GetMapping("/anime/genre")
-    public List<Serie> getAllByGenre(){
+
+    @GetMapping("/anime/top")
+    public  List<Integer> getAnimeByRating(@RequestParam(required = false) Optional<Integer> limit, @RequestParam(required = false) Optional<String> genre, @RequestParam(required = false) Optional<String> type, @RequestParam(required = false) Optional<String> studio,@RequestParam(required = false) Optional<String> source,@RequestParam(required = false) Optional<String> mainCast) {
+
         List<Serie> series= this.serieRepository.findAll();
-        return series;
+        return series.stream()
+                .filter(x-> x.getGenre().contains(genre.orElseGet(()->"")) )
+                .filter(x-> x.getGenre().contains(type.orElseGet(()->"")) )
+                .filter(x-> x.getGenre().contains(studio.orElseGet(()->"")) )
+                .filter(x-> x.getGenre().contains(source.orElseGet(()->"")) )
+                .filter(x-> x.getGenre().contains(mainCast.orElseGet(()->"")) )
+                .sorted(Comparator.comparingDouble(Serie::getRating).reversed())
+                .map(Serie::getAnime_id)
 
+                .limit(limit.orElseGet(()->series.size()))
+
+                .collect(Collectors.toList())
+                ;
 
     }
-    @GetMapping("/anime/{id}")
-    public Serie getSerieById(@PathVariable("{anime_id}") Integer id){
-        Serie serie =this.serieRepository.findByIds(id);
-        return serie;
-    }
+
+
 }
